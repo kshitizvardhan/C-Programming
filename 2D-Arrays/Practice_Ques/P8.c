2251a09ee50e687e4a2cb9c07146aefa550a92b0
kshitizvardhan <kshitizvardhan8@gmail.com>
2024-02-22 19:58:08 +0530

    P8-The row with maximum sum

---
+++ 2D-Arrays/Practice_Ques/P8.c
@@ -0,0 +1,25 @@
+// #include <stdio.h>
+
+// int main(){
+//     int r,c,index,max_sum=0;
+//     printf("Enter the rows: ");
+//     scanf("%d",&r);
+//     printf("Enter the columns: ");
+//     scanf("%d",&c);
+//     int arr[r][c];
+//     for (int i=0; i<r; i++){
+//         for (int j=0; j<c; j++){
+//             scanf("%d",&arr[i][j]);
+//         }
+//     }
+//     for (int i=0; i<r; i++){
+//         int sum = 0; 
+//         for (int j=0; j<c; j++){
+//             sum += arr[i][j];
+//             printf("%d",sum);
+//         }
+
+//     }
+//     printf("The maximum sum of the element of 2D array in %dth row is: %d",index,max_sum);
+//     return 0;
+// }
