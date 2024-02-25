32e25111f990f0ac528fad95ff3dba426f64d94e
kshitizvardhan <kshitizvardhan8@gmail.com>
2024-02-25 22:50:04 +0530

    Initialization of strings

--- Strings/Initialization.c
+++ Strings/Initialization.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 
 int main(){
-    char arr[] = {'a','t','l','a','s','\0'};
-    printf("%c",arr[4]);         // Putting 5 here does not prints anything as last index holds the null character which is not printed
+    char arr[] = {'a','t','l','a','s','\0'}; // Putting arr[5] here, does not prints anything as last index holds the null character which is not printed
+    int i = 0;
+    while (arr[i]!='\0'){
+        printf("%c",arr[i]);
+        i++;
+    }         
     printf("\n");
     // Shortcut Initialization
     char name[] = "Kshitiz";
