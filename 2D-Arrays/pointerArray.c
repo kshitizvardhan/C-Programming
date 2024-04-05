f680522416d17bca3a6614d5c40db5ed9f4a6f23
kshitizvardhan <kshitizvardhan8@gmail.com>
2024-04-05 10:10:47 +0530

    Largest element in the array

--- 2D-Arrays/pointerArray.c
+++ 2D-Arrays/pointerArray.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <limits.h>
 
 void double1(int arr[], int n){
     for(int i=0; i<n; i++){
@@ -6,11 +7,24 @@ void double1(int arr[], int n){
     }
 }
 
+void largest(int arr[], int n, int *ptr){
+    for(int i=0; i<n; i++){
+        if(arr[i]>*ptr){
+            *ptr = arr[i];
+        }
+    }
+}
+
 int main(){
     int arr[3] = {1,2,3};
     double1(arr,3);
+    int max = INT_MIN;
+    int *ptr = &max;
+    largest(arr,3,ptr);
     for(int i=0; i<3; i++){
         printf("%d ",arr[i]);
     }
+    printf("\n");
+    printf("The largest element in the array is: %d",max);
     return 0;
 }
